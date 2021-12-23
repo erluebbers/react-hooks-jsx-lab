a203cc36cd61c617bb95b0d3e1123867fef28a77
Erik Luebbers <erluebbers@gmail.com>
2021-12-22 17:03:53 -0800

    commit tree 5a99fbc6c0bb2337c2863d5e31c4f520b32d4891

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Home from "Home";
+import About from "About";
+
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    <a href={Home}>Home</a>
+    <a href={About}>About</a>
+  </nav>);
 }
 
 export default NavBar;
